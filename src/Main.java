7285b0cadf0d708089395b8baab8eec14255981e
Younes Karim <cph-yk31@cphbusiness.dk>
2021-02-02 13:02:16 +0100

    modification to structure

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> myNums = new ArrayList<>();
+        Lottery l = new Lottery();
         Scanner input = new Scanner(System.in);
+        ArrayList<Integer> myNums = new ArrayList<>();
+
         System.out.println("Indtast de numre du vil have i lotteriet!");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             int in = input.nextInt();
             myNums.add(in);
         }
 
-        Lottery l = new Lottery();
-        l.setNumbersToSet(5);
+        l.setNumbersToSet(4);
         l.addNumbersToLottery();
         System.out.println("Mine gæt:\t\t"+ myNums);
         System.out.println("Gevinst: "+l.lotteryGuess(myNums)+" kr");
-
     }
 }
